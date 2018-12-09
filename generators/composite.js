61d7e939f41ce6d0f50561db07bbd0558cd4cf64
AlexanderKotinov <alexander777ck@gmail.com>
2018-12-09 10:52:19 +0200

    Generated correct svg.

--- generators/composite.js
+++ generators/composite.js
@@ -2,24 +2,52 @@ const d3 = require('d3');
 const fetch = require('node-fetch');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+const fs = require('fs');
 var DOMParser = require('xmldom').DOMParser;
 
 
 module.exports = {
-  compositeGenerator: function (x, y) {
-    const dom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`);
-    let body = d3.select(dom.window.document.querySelector('body'));
+  compositeGenerator: function (type, coords) {
+    const dom = new JSDOM(`<!DOCTYPE html><html><body>
+${fs.readFileSync('./assets/pdi_results_composite.svg', 'utf-8')}
+</body></html>`);
     const height = 600;
     const width = 240;
-    let svg = body.append('svg')
-      .attr("version", "1.1")
-      .attr("xmlns", d3.namespaces.svg)
-      .attr("xmlns:xlink", d3.namespaces.xlink)
-      .attr("width", width)
-      .attr("height", height);
-    return new Promise(function(resolve, reject) {
-      resolve(d3.svg('http://localhost:3003/assets/pdi_results_composite.svg'));
-    });
+
+    switch(type) {
+      case 'composite':
+
+    }
+
+    let svg = d3.select(dom.window.document.querySelector('svg'));
+    // let svg = body.append('svg')
+    //   .attr("version", "1.1")
+    //   .attr("xmlns", d3.namespaces.svg)
+    //   .attr("xmlns:xlink", d3.namespaces.xlink)
+    //   .attr("width", width)
+    //   .attr("height", height);
+
+    // body.html(fs.readFileSync('./assets/pdi_results_composite.svg', 'utf-8'));
+
+    // let svg =
+    // let svg = body.node().getElementsByTagName('svg')[0];
+    //
+      svg.attr('width', width)
+      .attr('height', height)
+      .style('width', width)
+      .style('height', height).append('g').append('circle')
+      .attr('cx', 200)
+      .attr('cy', 500)
+      .attr('r', 20)
+      .attr('fill', 'orange');
+
+    // let body = d3.select(dom.window.document.querySelector('body'));
+    // body.node().innerHTML = '';
+    // body.append(svg.node());
+    return svg.node().outerHTML;
+    // return new Promise(function(resolve, reject) {
+    //   resolve(d3.svg('http://localhost:3003/assets/pdi_results_composite.svg'));
+    // });
     //return d3.svg('http://localhost:3003/assets/pdi_results_composite.svg');
     // background.then(data => {
     //   svg.html(data.documentElement.previousSibling);
--- server.js
+++ server.js
@@ -2,23 +2,21 @@ const express = require('express');
 const app = express();
 const request = require('request-promise');
 const composite = require('./generators/composite');
+const env = require('./.env');
 
 const PORT = 3003;
 
 
-app.get('/composite', (req, res) => {
-  composite.compositeGenerator(req.query.x, req.query.y).then(data => {
-    return res.status(200).send(data);
-  }).catch(() => {
-    return res.status(400);
-  });
+app.get('/generate', (req, res) => {
+  const data = composite.compositeGenerator(req.query.type, req.query.coords);
+  return res.status(200).send(data);
 });
 
 app.use('/assets', express.static(__dirname + '/assets'));
 
 app.listen(PORT, () => {
   if (typeof fetch !== 'function') {
-    global.fetch = require('node-fetch')
+    global.fetch = require('node-fetch');
     global.DOMParser = require('xmldom').DOMParser;
   }
   console.log('Server ready!');
